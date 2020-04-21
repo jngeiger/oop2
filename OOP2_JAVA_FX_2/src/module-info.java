e2a99ea121d28c10041ea27fafbe5becc20531e2
jan <jan@jans-mbp.intern.geiger-digital.de>
2020-04-21 16:36:12 +0200

    push

--- OOP2_JAVA_FX_2/src/module-info.java
+++ OOP2_JAVA_FX_2/src/module-info.java
@@ -1,7 +1,7 @@
 module OOP2_JAVA_FX_2 {
 	requires javafx.base;
-	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.controls;
 	exports application;
 	opens application to javafx.fxml;
 }
