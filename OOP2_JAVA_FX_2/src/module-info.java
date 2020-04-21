297439ec10f7022ae230b14f2eb14cd03dcf86ab
jan <jan@jans-mbp.intern.geiger-digital.de>
2020-04-21 22:30:57 +0200

    test

--- OOP2_JAVA_FX_2/src/module-info.java
+++ OOP2_JAVA_FX_2/src/module-info.java
@@ -2,6 +2,8 @@ module OOP2_JAVA_FX_2 {
 	requires javafx.base;
 	requires javafx.fxml;
 	requires javafx.controls;
+	requires javafx.graphics;
+	
 	exports application;
 	opens application to javafx.fxml;
 }
---
+++ OOP2_JAVA_FX_2/src/stopwatch/StopwatchController.java
@@ -0,0 +1,12 @@
+package stopwatch;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class StopwatchController {
+	
+	@FXML
+    void printHello(ActionEvent event) {
+			System.out.println("Hello World");
+    }
+}
