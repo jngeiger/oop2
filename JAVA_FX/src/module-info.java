49c671351e9980e52f6f70d7d8b295500ad03cb0
Jan <Jan@JAN-PC.intern.geiger-digital.de>
2020-05-05 13:41:39 +0200

    push

---
+++ JAVA_FX/src/module-info.java
@@ -0,0 +1,10 @@
+module OOP2_JAVA_FX_2 {
+	requires javafx.base;
+	requires javafx.fxml;
+	requires javafx.controls;
+	requires javafx.graphics;
+	
+	exports oop2.schachtel;
+	opens oop2.schachtel;
+
+}
---
+++ JAVA_FX/src/oop2/schachtel/SchachtelController.java
@@ -0,0 +1,12 @@
+package oop2.schachtel;
+
+import javafx.fxml.FXML;
+
+public class SchachtelController {
+
+	@FXML
+	void exit()
+	{
+		System.exit(0);
+	}
+}
