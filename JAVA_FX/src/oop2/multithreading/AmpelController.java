bbe1a7d269a07735e73fe33bf49fdc90d4f21b65
Jan <Jan@JAN-PC.intern.geiger-digital.de>
2020-06-17 15:50:45 +0200

    push

--- JAVA_FX/src/oop2/multithreading/AmpelController.java
+++ JAVA_FX/src/oop2/multithreading/AmpelController.java
@@ -3,10 +3,13 @@ package oop2.multithreading;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
@@ -17,6 +20,8 @@ public class AmpelController implements Initializable {
 @FXML Button btn1;
 @FXML Button btn2;
 Thread t1;
+Service<Void> s1;
+@FXML TextField tf1;
 
 @Override
 public void initialize(URL location, ResourceBundle resources) {
@@ -41,39 +46,47 @@ public void initialize(URL location, ResourceBundle resources) {
 	c3.setCenterY(100.0f);
 	c3.setStroke(Color.BLACK);
 	c3.setRadius(33.0f); 
+	
+	
 }
 
 public void start(ActionEvent event) 
 {
-	t1 = new Thread() {
-		public void run()
+	
+	s1 = new Service<Void>() {
+		protected Task<Void> createTask()
 		{
-			while (true)
-			{
-				try {
-					c3.setFill(Color.TRANSPARENT);
-					c1.setFill(Color.RED);
-					Thread.sleep(1000);
-					c1.setFill(Color.TRANSPARENT);
-					c2.setFill(Color.YELLOW);
-					Thread.sleep(1000);
-					c2.setFill(Color.TRANSPARENT);
-					c3.setFill(Color.GREEN);
-					Thread.sleep(1000);
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+			return new Task<Void>() {
+				protected Void call() throws InterruptedException
+				{
+					tf1.setText("STARTED");
+					while (!isCancelled())
+					{
+						
+							c3.setFill(Color.TRANSPARENT);
+							c1.setFill(Color.RED);
+							Thread.sleep(1000);
+							c1.setFill(Color.TRANSPARENT);
+							c2.setFill(Color.YELLOW);
+							Thread.sleep(1000);
+							c2.setFill(Color.TRANSPARENT);
+							c3.setFill(Color.GREEN);
+							Thread.sleep(1000);
+						
+						
+					}
+					return null;
 				}
-				
-			}
-		}
+			};
+		};
 	};
-	t1.start();
+	s1.start();
 }
 
 public void stop(ActionEvent event)
 {
-	t1.stop();
+	s1.cancel();
+	this.initialize(null, null);
 }
 
 }
