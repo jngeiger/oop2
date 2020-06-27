281d435eef350c9c70aacdc0fedfcb4117721b95
Jan-Niklas Geiger <jan@jans-mbp.intern.geiger-digital.de>
2020-06-27 08:46:36 +0200

    push

--- JAVA_FX/src/oop2/statistik/Menu1Controller.java
+++ JAVA_FX/src/oop2/statistik/Menu1Controller.java
@@ -7,6 +7,9 @@ import java.util.ResourceBundle;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -15,105 +18,91 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class Menu1Controller implements Initializable {
-@FXML GridPane root;
-@FXML TextArea ta;
-ArrayList<Integer> rawData = new ArrayList<Integer>();
-@FXML Label validData;
-SimpleBooleanProperty isValid = new SimpleBooleanProperty();
-@FXML TextField tfVar;
-@FXML TextField tfStandard;
-Statistics engine = new Statistics();
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
-		validData.setStyle("-fx-background-color: green;");
-		
-		
-		ta.textProperty().addListener(new ChangeListener<String>() {
-			@Override
-			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				// TODO Auto-generated method stub
-				rawData.clear();
-				isValid.set(true);
-				int idx = 0;
-				
-				for (int i = 0; i < newValue.length(); i++)
-				{
-					if (newValue.length()-1 == i)
-					{
-						try {
-						rawData.add(Integer.valueOf(newValue.substring(idx,i+1)));
-						}
-						catch (Exception e)
-						{
-							isValid.set(false);;
-						}
-					}
-					if (newValue.charAt(i) == ',')
-					{
-						try {
-						rawData.add(Integer.valueOf(newValue.substring(idx,i)));
-						idx = i+1;
-						}
-						catch (Exception e)
-						{
-							isValid.set(false);;
-						}
-					}
-				}
-				engine.getVarianz(rawData);
-				engine.getStandardabweichung(rawData);
-				
-				
-			}});
-		
-		
-		
-		isValid.addListener(new ChangeListener<Boolean>() {
-			public void changed(ObservableValue<? extends Boolean> obs, Boolean old, Boolean newVal)
-			{
-				if (isValid.get() == true)
-				{
-					validData.setStyle("-fx-background-color: green;");
-					validData.setText("Gültige Eingabe");
-				}
-				else {
-					validData.setStyle("-fx-background-color: red;");
-					validData.setText("Ungültige Eingabe");
-				}
-			}
-		});
-		
-		tfVar.textProperty().bind(engine.getVarianz(rawData));
-		tfStandard.textProperty().bind(engine.getStandardabweichung(rawData));
-		
-		
-		}
+    @FXML GridPane root;
+    @FXML TextArea ta;
+    ObservableList<Integer> rawData = FXCollections.observableList(new ArrayList<Integer>());
+    @FXML Label validData;
+    SimpleBooleanProperty isValid = new SimpleBooleanProperty();
+    @FXML TextField tfVar;
+    @FXML TextField tfStandard;
+    @FXML TextField tfModus;
+    @FXML TextField tfMedian;
+    @FXML TextField tfMittelwert;
+    ImageView iV;
+    Image hacken;
+    Image kreuz;
+    Statistics engine = new Statistics(rawData);
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        iV = new ImageView();
+        hacken = new Image(getClass().getResourceAsStream("hackenPNG.png"),75.,75.,true,true);
+        kreuz = new Image(getClass().getResourceAsStream("XPNG.png"),50.,50.,true,true);
+        iV.setImage(hacken);
+        validData.setGraphic(iV);
+        ta.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                // TODO Auto-generated method stub
+                rawData.clear();
+                isValid.set(true);
+                int idx = 0;
 
-	
-	
-	
-	
-	public void calculate(ActionEvent event)
-	{
-		engine.getVarianz(rawData);
-		try {
-			if (isValid.get() == false)
-			{
-				Alert a = new Alert(AlertType.ERROR);
-				a.showAndWait();
-				
-			}
-			
-		}
-		catch (Exception e)
-		{
-			
-		}
-		
-	}
+                for (int i = 0; i < newValue.length(); i++)
+                {
+                    if (newValue.length()-1 == i)
+                    {
+                        try {
+                            rawData.add(Integer.valueOf(newValue.substring(idx,i+1)));
+                        }
+                        catch (Exception e)
+                        {
+                            isValid.set(false);
+                        }
+                    }
+                    if (newValue.charAt(i) == ',')
+                    {
+                        try {
+                            rawData.add(Integer.valueOf(newValue.substring(idx,i)));
+                            idx = i+1;
+                        }
+                        catch (Exception e)
+                        {
+                            isValid.set(false);
+                        }
+                    }
+                }
+                engine.getVarianz();
+                engine.getStandardabweichung();
+                engine.getMittelwert();
+
+            }});
+        isValid.addListener(new ChangeListener<Boolean>() {
+            public void changed(ObservableValue<? extends Boolean> obs, Boolean old, Boolean newVal)
+            {
+            	
+						
+						if (isValid.get() == true)
+		                {
+		                    iV.setImage(hacken);
+		                }
+		                else {
+		                    iV.setImage(kreuz);
+		                }
+					
+            }
+        });
+        tfMittelwert.textProperty().bind(engine.getMittelwert());
+        tfVar.textProperty().bind(engine.getVarianz());
+        tfStandard.textProperty().bind(engine.getStandardabweichung());
+    }
+    public void reset(ActionEvent actionEvent)
+    {
+        ta.setText("");
+    }
 }
--- JAVA_FX/src/oop2/statistik/Statistics.java
+++ JAVA_FX/src/oop2/statistik/Statistics.java
@@ -1,33 +1,56 @@
 package oop2.statistik;
 
-import java.util.List;
+
 
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+
+import java.util.List;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+
 public class Statistics {
-private SimpleStringProperty varianz = new SimpleStringProperty();
-private SimpleStringProperty standardabw = new SimpleStringProperty();
-	
-	public SimpleStringProperty getVarianz(List<Integer> rawData)
-	{
-		Integer n = rawData.size();
-		Double sum = 0.0;
-		Double sumsquared = 0.0; 
-			
-		for (Integer x : rawData)
-		{
-			sum += x.doubleValue();
-			sumsquared += x.doubleValue()*x.doubleValue();
-		}
-		varianz.set(String.valueOf((sumsquared - ((sum*sum)/n))/n));
-		return varianz;
-		
-	}
-	
-	public SimpleStringProperty getStandardabweichung(List<Integer> rawData)
-	{
-	    standardabw.set(String.valueOf((Math.sqrt(Double.valueOf(getVarianz(rawData).get())))));
-	    return standardabw;
-	}
+    private final ObservableList<Integer> rawData;
+    private final SimpleStringProperty varianz = new SimpleStringProperty();
+    private final SimpleStringProperty standardabw = new SimpleStringProperty();
+    private final SimpleStringProperty mittelwert = new SimpleStringProperty();
+    public Statistics(ObservableList<Integer> rawData)
+    {
+            this.rawData = rawData;
+    }
+    public SimpleStringProperty getVarianz()
+    {
+        Integer n = rawData.size();
+        Double sum = 0.0;
+        Double sumsquared = 0.0;
+
+        for (Integer x : rawData)
+        {
+            sum += x.doubleValue();
+            sumsquared += x.doubleValue()*x.doubleValue();
+        }
+        varianz.set(String.valueOf((sumsquared - ((sum*sum)/n))/n));
+        return varianz;
+
+    }
+
+    public SimpleStringProperty getStandardabweichung()
+    {
+        standardabw.set(String.valueOf((Math.sqrt(Double.valueOf(getVarianz().get())))));
+        return standardabw;
+    }
+    public SimpleStringProperty getMittelwert()
+    {
+        Integer n = rawData.size();
+        Double sum = 0.;
+        for(Integer x : rawData){
+            sum += x.doubleValue();
+        }
+        mittelwert.set(String.valueOf(sum/n));
+        return mittelwert;
+    }
 }
